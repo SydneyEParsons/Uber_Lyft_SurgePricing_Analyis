7115bca5fafb89abc38606c6db93bf18cf9f47d5
Sydney Parsons <r2sydney@gmail.com>
2026-02-16 15:19:09 -0400

    Edited the merge code. The data is not converted to Unix time and specifies the day. It check for duplicates and saves the csv file of the merged cab_rides and weather data.

--- R/hello.R
+++ R/hello.R
@@ -1,14 +1,28 @@
+# Load the data files
+cab_rides <- read.csv("data/cab_rides.csv")
+weather <- read.csv("data/weather.csv")
 
 # Checking Unique Values --------------------------------------------------
 length(unique(cab_rides$time_stamp))
 length(unique(weather$time_stamp))
 
+# Check for duplicates before cleaning
+cat("Cab rides before:", nrow(cab_rides), "\n")
+cat("Duplicates found:", sum(duplicated(cab_rides)), "\n")
+
+#Removing taxi
+cab_rides <- cab_rides %>%
+  filter(name != "Taxi")
+
 
 # Merging A Range of Weather & Rides --------------------------------------
 library(data.table)
+library(dplyr)
+library(lubridate)
 
 cab_rides <- cab_rides %>%
-  mutate(time_stamp = floor(time_stamp / 1000))
+  mutate(time_stamp = as.POSIXct(time_stamp / 1000, origin = "1970-01-01", tz = "UTC"), hour_ts = floor_date(time_stamp, unit = "hour"))
+
 
 setDT(cab_rides)
 setDT(weather)
@@ -16,14 +30,13 @@ setDT(weather)
 setkey(cab_rides, time_stamp)
 setkey(weather, time_stamp)
 
-merged_data <- weather[cab_rides, roll = "nearest"]
-
-merged_data <- merged_data %>%
-  filter(name != "Taxi")
+merged_data <- weather[cab_rides, roll = "nearest", allow.cartesian = TRUE]
 
 dim(merged_data)
 head(merged_data)
 print(merged_data)
 
 write.csv(merged_data, "merged_data_cleaned.csv", row.names = FALSE)
+
+
 
