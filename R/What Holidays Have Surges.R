d9d3b4fa3bb9154149e8464f24ce9f69bf04c55b
Sydney Parsons <r2sydney@gmail.com>
2026-02-23 22:57:10 -0400

    Adding filters for Hanukkah and Cyber Monday

--- R/What Holidays Have Surges.R
+++ R/What Holidays Have Surges.R
@@ -1,3 +1,17 @@
 #What Holidays Have Surges
+
+# Load libraries
+library(dplyr)
+library(lubridate)
+
+# Load merged data
+merged_data <- read.csv("merged_data_cleaned.csv")
+
+# Extracting the date from the time stamp
 merged_data <- merged_data %>%
   mutate(date = as.Date(time_stamp))
+head(merged_data$date)
+
+#Hanukkah
+
+#Cyber Monday
