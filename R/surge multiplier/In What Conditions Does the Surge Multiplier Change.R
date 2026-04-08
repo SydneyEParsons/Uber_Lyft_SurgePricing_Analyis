d0e63ed215224c628b175e62d062672864b178e5
Sydney Parsons <sydney@dal.ca>
2026-04-08 18:41:41 -0300

    Cleaning up folders, fixing time for rush hour.

--- R/surge multiplier/In What Conditions Does the Surge Multiplier Change.R
+++
@@ -1,2 +0,0 @@
-#In What Conditions Does the Surge Multiplier Change
-#After teh other analyses are done we could add to this to show what overall affects the surge multiplier.
--- R/weather/Analysis of Surge Prices Relating to Weather.R
+++
@@ -1,1 +0,0 @@
-#Analysis of Surge Prices Relating to Weather
